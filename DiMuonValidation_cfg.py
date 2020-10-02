f00b003b0ca455d5468472b661219f5cb87a28a5
An Gu <32967063+gu180@users.noreply.github.com>
2020-10-02 09:44:50 -0400

    Update DiMuonValidation_cfg.py

--- DiMuonValidation_cfg.py
+++ DiMuonValidation_cfg.py
@@ -7,6 +7,7 @@ process.load("Configuration.StandardSequences.Services_cff")
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 import CalibTracker.Configuration.Common.PoolDBESSource_cfi  
 process.GlobalTag.globaltag = '102X_upgrade2018_realistic_v10'
 process.GlobalTag.toGet = cms.VPSet(
@@ -39,7 +40,10 @@ process.TrackRefitter1 = process.TrackRefitterP5.clone(
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
 process.myanalysis = cms.EDAnalyzer('DiMuonValidation',
-                                     TkTag = cms.string ('ALCARECOTkAlZMuMu'),#TrackRefitter1
+                                     TkTag = cms.string ('TrackRefitter1'),
+                                     #TkTag = cms.string ('ALCARECOTkAlZMuMu'),#TrackRefitter1
+                                     Pair_mass_min = cms.double(80),
+                                     Pair_mass_max = cms.double(120)
                               )
 process.TFileService = cms.Service("TFileService",
       fileName = cms.string("DiMuonValidation_output.root"),
