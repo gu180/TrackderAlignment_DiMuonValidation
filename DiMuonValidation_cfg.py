cbb74d64ac22e292f68d49c1c3f3e8cff91933fd
An Gu <32967063+gu180@users.noreply.github.com>
2020-11-18 02:52:36 -0500

    Update DiMuonValidation_cfg.py

--- DiMuonValidation_cfg.py
+++ DiMuonValidation_cfg.py
@@ -7,17 +7,18 @@ process.load("Configuration.StandardSequences.Services_cff")
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 import CalibTracker.Configuration.Common.PoolDBESSource_cfi  
-process.GlobalTag.globaltag = '102X_upgrade2018_realistic_v10'
+#process.GlobalTag.globaltag = '102X_upgrade2018_realistic_v10'#92X_dataRun2_Prompt_v11 110X_dataRun2_v13
+process.GlobalTag.globaltag = '92X_dataRun2_Prompt_v11'
+"""
 process.GlobalTag.toGet = cms.VPSet(
    cms.PSet(record = cms.string('TrackerAlignmentRcd'),
             tag = cms.string('TrackerAlignment_MC2018_v1'),
             connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
             ),
 )
-
-
-
+"""
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(
@@ -37,9 +38,28 @@ process.TrackRefitter1 = process.TrackRefitterP5.clone(
     )
 ###############################################################################
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.myanalysis = cms.EDAnalyzer('DiMuonValidation',
-                                     TkTag = cms.string ('ALCARECOTkAlZMuMu'),#TrackRefitter1
+                                     TkTag = cms.string ('TrackRefitter1'),
+                                     #TkTag = cms.string ('ALCARECOTkAlZMuMu'),#TrackRefitter1
+                                     Pair_mass_min = cms.double(80),
+                                     Pair_mass_max = cms.double(120),
+                                     Pair_eta_min = cms.double(-2.5),
+                                     Pair_eta_max = cms.double(+2.5),
+                                     Pair_pt_min = cms.double(0),
+                                     Pair_pt_max = cms.double(1000),
+                                     #Variable_CosThetaCS_nbins = cms.int(20)
+                                     Variable_CosThetaCS_xmin = cms.double(-1),
+                                     Variable_CosThetaCS_xmax = cms.double(+1),
+                                     #Variable_PairPt_nbins= cms.int(100),
+                                     Variable_PairPt_xmin = cms.double(0),
+                                     Variable_PairPt_xmax = cms.double(100),
+                                     Variable_DeltaEta_xmin = cms.double(-4.8),
+                                     Variable_DeltaEta_xmax = cms.double(+4.8),
+                                     Variable_EtaPlus_xmin = cms.double(-2.4),
+                                     Variable_EtaPlus_xmax = cms.double(+2.4),
+                                     Variable_EtaMinus_xmin = cms.double(-2.4),
+                                     Variable_EtaMinus_xmax = cms.double(+2.4)
                               )
 process.TFileService = cms.Service("TFileService",
       fileName = cms.string("DiMuonValidation_output.root"),
